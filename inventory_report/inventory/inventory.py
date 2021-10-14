22e6f459d412511a2989cb2de57b24ca12bb2828
allenhichard <allenhichard21@gmail.com>
2021-10-14 00:28:24 -0300

    Implementação do requisito 3. Criação do método import_data que realiza a leitura de arquivos com extensão (Json, Csv, e Xml) e gera dois tipos de relatórios (Simples ou Completo) de acordo com a preferência do usuário.

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from inventory_report.reports import simple_report, complete_report
+
+class Inventory:
+    
+    
+    '''
+    Este método capaz de ler um arquivo CSV, JSOM ou XML o qual o caminho é passado como parâmetro
+    junto com o tipo de relatório (Simples ou Completo). De acordo com os parâmetros recebidos, 
+    deve recuperar os dados do arquivo e chamar o método de gerar relatório correspondente à 
+    entrada passada
+    '''
+    
+    @staticmethod
+    def import_data(path, reportType):
+        if path.endswith('.csv'):
+            file = pd.read_csv("../"+path)
+        elif path.endswith('.json'):
+            file = pd.read_json("../" + path)
+        else:
+            file = pd.read_xml("../" + path)
+        exe = {"simples": simple_report.SimpleReport, "completo": complete_report.CompleteReport}
+        return exe[reportType].generate(file)
+    
+    
+        
