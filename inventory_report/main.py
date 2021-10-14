31a18e737bc5f664e574ba2d8b85ede1c5f041ed
Allen Hichard <53483318+AllenHichard@users.noreply.github.com>
2021-10-14 15:19:19 -0300

    Acrescentando duas linhas em branco

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -3,6 +3,8 @@ from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.inventory.inventory_refactor import InventoryRefactor
 import sys
+
+
 def main():
     if len(sys.argv) < 3:
         sys.stderr.write("Verifique os argumentos\n")
